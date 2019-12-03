30ac1a1830e64088b6c4aaa908e86df65572f18b
Asierso <33327643+Asierso@users.noreply.github.com>
2019-12-03 01:57:03 +0100

    1.0

---
+++ KDSME-STABLE/main.cs
@@ -0,0 +1,12 @@
+using System;
+namespace main_namespace
+{
+    class main_class
+    {
+        public static void Main()
+        {
+            Console.Write("KDSME-TOOLS");
+            Console.Write("Nothing:");
+        }
+    }
+}
